14518d1f183a6b000fda26b36bfd9160c2013a5c
Jordan Dines <jordan@dines.net.au>
2018-03-14 14:46:41 +1100

    Added Jordan's Shitbox Added GoogleVR Added Player Prefab

---
+++ Assets/3rdPartyAssets/GoogleVR/_Prefab/GryoRotate.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GryoRotate : MonoBehaviour {
+
+	private Gyroscope gyro;
+
+	void Awake () {
+		if (SystemInfo.supportsGyroscope)
+		{
+			gyro = Input.gyro;
+			gyro.enabled = true;
+		}
+		else
+		{
+			Debug.Log("Phone doesen't support");
+		}
+	}
+
+	void Update () 
+	{
+		GameObject player = GameObject.FindGameObjectWithTag ("Player");
+		player.transform.Rotate (-gyro.rotationRateUnbiased.x, -gyro.rotationRateUnbiased.y, gyro.rotationRateUnbiased.z);
+	}
+
+	void OnGUI()
+	{
+		GUILayout.Label ("Gyroscope attitude : " + gyro.attitude);
+	}
+}
+
---
+++ Assets/Scripts/TriggerableObject.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerableObject : MonoBehaviour {
+	
+	public Vector3 deactivatedPos;  // position for door at closed position
+	public Vector3 activatedPos;    // position for door at opened position
+
+	public bool gazedAtNow = false; // whether the door is beign gazed at or not
+
+
+	void Start () {
+		deactivatedPos = transform.position;
+		activatedPos = transform.position + (Vector3.up * 2f);
+	}
+	
+
+	void Update () {
+	
+		if (gazedAtNow) {  // if it's being gazed at
+			if (transform.position.y < activatedPos.y){
+			transform.position = transform.position + (Vector3.up * 1f * Time.deltaTime);
+			}
+		} else {           // if it's not being gazed at
+			if (transform.position.y > deactivatedPos.y) {
+				transform.position = transform.position + (Vector3.down * 0.3f * Time.deltaTime);
+			}
+		}
+
+		// if door closes again, set to white
+		if (transform.position.y <= deactivatedPos.y){
+		GetComponent<Renderer> ().material.color = Color.white;
+		}
+	}
+
+
+
+	// these functions are called from the Event Trigger on the door, on PointerEnter and PointerExit
+
+	// if gazed at, change color to blue, and set gazedAtNow = true
+	public void ActivateNow(){
+		GetComponent<Renderer> ().material.color = Color.blue;
+		gazedAtNow = true;
+
+	}
+
+
+	// if not gazed at, set gazedAtNow = false
+	public void DeactivateNow(){
+		
+		gazedAtNow = false;
+
+	}
+
+
+}
