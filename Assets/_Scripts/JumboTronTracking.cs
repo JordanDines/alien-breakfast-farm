b5821fdfb39cc18fb2fd99e73007f1f31cd1f29a
Jarrod Sevior <jarrod.sevior@gmail.com>
2018-03-20 09:51:32 +1100

    Added: -SCRIPTS- -JumboTronTracking
    Modified:
    -MESHES-
    - Coffee Machine
    - Stove
    - Toaster
    -PREFABS-
    - Coffee Machine
    - Stove
    - Toaster
    -SCENES-
    - Jarrod's Sandbox

---
+++ Assets/_Scripts/JumboTronTracking.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumboTronTracking : MonoBehaviour 
+{
+	public float rotateSpeed;
+	private Vector3 targetRotation;
+
+	public GameObject player;
+
+
+	void Start ()
+	{
+	
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.rotation = Quaternion.Lerp (transform.rotation, player.transform.rotation, rotateSpeed);
+	}
+
+}
