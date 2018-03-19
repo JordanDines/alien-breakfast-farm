87f03775904cde763fa6c14a341c873b6e322385
Jordan Dines <jordan@dines.net.au>
2018-03-19 11:47:21 +1100

    Updated Jordan's Shitbox. Created new Player (not yet prefabed) Needs Testing Added Legacy GoogleVR Scripts

---
+++ Assets/ReticleTest.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReticleTest : MonoBehaviour {
+
+	public void TestFunction () {
+		Debug.Log ("Getem");
+	}
+}
