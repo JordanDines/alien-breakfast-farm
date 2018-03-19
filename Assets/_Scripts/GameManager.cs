d23e666cae6f267ccfccc77cdbdb5d8b044b12e3
Jordan Dines <jordan@dines.net.au>
2018-03-19 16:55:46 +1100

    Updated Player Prefab Updated Scripts Folder Updated Jordan's Shitbox

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
+
+
 	// Use this for initialization
 	void Start () {
 		Screen.orientation = ScreenOrientation.LandscapeLeft;
---
+++ Assets/_Scripts/ObjectInteract.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectInteract : MonoBehaviour {
+
+	public GameObject holdingPoint;
+	public GameObject objectPoint;
+	public float grabbingSpeed = 1;
+	public GameObject holdingParent;
+
+	public bool isMoving = false;
+	public bool isHeld = false;
+	public bool isPlacing = false;
+
+	private GameManager gameManager;
+
+	void Start () {
+		holdingPoint = GameObject.FindGameObjectWithTag ("HoldingPoint");
+	}
+
+	void Update () {
+		MoveTowardsPlayer ();
+		MoveTowardsPlacement ();
+		HoldingObject ();
+
+		print (holdingPoint.transform.childCount);
+	}
+
+
+	#region BUTTONS
+	public void GrabObject () {
+		if (isHeld && transform.tag == "Interactable") {
+			isHeld = false;
+			isPlacing = true;
+			transform.SetParent (null);
+			Debug.Log ("HIT" + transform.name);
+		} else if(!isHeld && transform.tag == "Box") {
+			isMoving = true;
+			transform.SetParent (holdingParent.transform);
+		}
+		else if (isHeld) {
+			isHeld = false;
+			transform.SetParent (null);
+			gameObject.GetComponent<Collider> ().enabled = true;
+			gameObject.GetComponent<Rigidbody> ().useGravity = true;
+		}
+	}
+	#endregion
+
+	#region PLAYER_INTERACTIONS
+	void MoveTowardsPlacement () {
+		if (isPlacing) {
+			transform.position = Vector3.Lerp (transform.position, objectPoint.transform.position, grabbingSpeed);
+			if (transform.position == objectPoint.transform.position) {
+				isPlacing = false;
+			}
+		}
+	}
+
+	void MoveTowardsPlayer () {
+		//Check is the object has been picked up
+		if (isMoving) {
+			//Turn off the rigidbody and collider
+			gameObject.GetComponent<Rigidbody> ().useGravity = false;
+			gameObject.GetComponent<Collider> ().enabled = false;
+			//Then move it to the player and make it look at the player
+			transform.position = Vector3.Lerp (transform.position, holdingPoint.transform.position, grabbingSpeed);
+			transform.LookAt(GameObject.FindGameObjectWithTag ("Player").transform.position);
+			//If it has reached it's holding point, allow it to be interactable
+			if (transform.position == holdingPoint.transform.position) {
+				isHeld = true;
+				isMoving = false;
+			}
+		}
+	}
+
+	void HoldingObject () {
+		if (isHeld) {
+			transform.LookAt (GameObject.FindGameObjectWithTag ("Player").transform.position);
+		}
+	}
+	#endregion
+		
+}
