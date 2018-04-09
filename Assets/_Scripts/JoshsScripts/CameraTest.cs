6029235bee46924f23797a34b007c150afd98e7f
Joshua D'Agostino <joshua.dagostino@outlook.com>
2018-04-09 17:53:03 +1000

    --- Changes --- - Merged changes form non VR version into main version
    - Performance improvements
    - Staple point fixes
    - Mushroom cooked now more than single slice

---
+++ Assets/_Scripts/JoshsScripts/CameraTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class CameraTest : MonoBehaviour {
+
+    public Camera Camera;
+
+	// Use this for initialization
+	void Start () {
+        UnityEngine.XR.XRSettings.enabled = false;
+
+        Camera = Camera.main;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+       Camera.GetComponent<Transform>().localRotation = UnityEngine.XR.InputTracking.GetLocalRotation(XRNode.CenterEye);
+    }
+}
