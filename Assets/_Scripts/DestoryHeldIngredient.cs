fc18cecc89f21b07a55932d241218c9b353feaf4
Jordan Dines <jordan@dines.net.au>
2018-04-10 11:49:58 +1000

    RUBBISH BIN ADDED

---
+++ Assets/_Scripts/DestoryHeldIngredient.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestoryHeldIngredient : MonoBehaviour {
+
+	private GameManager gm;
+
+	void Start () {
+		gm = FindObjectOfType<GameManager> ();
+	}
+
+	void Update () {
+		if (gm.holdingObject != null) {
+			GetComponent<Collider> ().enabled = true;
+		} else
+			GetComponent<Collider> ().enabled = false;
+	}
+
+	// Update is called once per frame
+	public void DestoryThisIngredient () {
+		gm.holdingObject.GetComponent<ObjectInteract> ().respawnParent.GetComponent<Respawn> ().RespawnIngredient ();
+		Destroy (gm.holdingObject.gameObject);
+		gm.holdingObject = null;
+		gm.canHold = true;
+		gm.canPlace = false;
+	}
+}
