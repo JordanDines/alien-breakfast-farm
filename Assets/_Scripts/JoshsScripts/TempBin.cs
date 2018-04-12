172c3f4aadaf22512b81eae08d76822c2de91289
Joshua D'Agostino <joshua.dagostino@outlook.com>
2018-04-12 19:40:44 +1000

    --- Updates --- - I got bored and made the bin open and close with a fire particle system that also turns off and on with it.

---
+++ Assets/_Scripts/JoshsScripts/TempBin.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempBin : MonoBehaviour {
+
+    public GameObject binUpper;
+    public GameObject binLower;
+
+    public GameObject obj1Pos1;
+    public GameObject obj1Pos2;
+
+    public GameObject obj2Pos1;
+    public GameObject obj2Pos2;
+
+    public ParticleSystem fire;
+
+    public float OpenCloseSpeed;
+    public float timeToWait;
+
+   [HideInInspector]
+    public float timeCount;
+
+    private bool timeForThing;
+    // Use this for initialization
+    void Start () {
+                
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (timeForThing == true)
+        {
+            DoTheThing();
+            timeCount += Time.deltaTime;
+          //  print("TimeCount is "+ timeCount);
+        }
+        else
+            timeCount = 0f;
+
+    }
+
+    public void DoTheThing() {
+
+        timeForThing = true;
+        print("Open");
+        binUpper.transform.position = Vector3.Lerp(obj1Pos1.transform.position, obj1Pos2.transform.position, OpenCloseSpeed);
+        binLower.transform.position = Vector3.Lerp(obj2Pos1.transform.position, obj2Pos2.transform.position, OpenCloseSpeed);
+        fire.gameObject.SetActive(true);
+
+        if (timeCount >= timeToWait) {
+            print("Close");
+            binUpper.transform.position = Vector3.Lerp(obj1Pos2.transform.position, obj1Pos1.transform.position, OpenCloseSpeed);
+            binLower.transform.position = Vector3.Lerp(obj2Pos2.transform.position, obj2Pos1.transform.position, OpenCloseSpeed);
+
+            fire.gameObject.SetActive(false);
+            timeForThing = false;
+        }
+    }
+
+
+}
