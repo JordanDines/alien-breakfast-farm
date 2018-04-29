84515246877d7e57a0a10e8e8fa0ea7c264aafd7
Joshua D'Agostino <joshua.dagostino@outlook.com>
2018-04-29 22:01:51 +1000

    --- Updates --- - Trying out way to move button. - Josh's scripts need to be redone/changed from Designer code to working progerammer code.

---
+++ Assets/_Scripts/JoshsScripts/ButtonMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonMovement : MonoBehaviour {
+
+    public GameObject button;
+
+    public GameObject buttonUpPos;
+    public GameObject buttonDownPos;
+
+    public float pressSpeed;
+
+   [HideInInspector]
+    public float timeCount;
+
+    private bool timeForThing;
+    // Use this for initialization
+    void Start () {
+                
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (timeForThing == true)
+        {
+            DoTheThing();
+            timeCount += Time.deltaTime;
+        }
+        else
+            timeCount = 0f;
+
+    }
+
+    public void DoTheThing() {
+
+        timeForThing = true;
+        print("Pressed");
+        button.transform.position = Vector3.MoveTowards(buttonUpPos.transform.position, buttonDownPos.transform.position, pressSpeed);
+ 
+        button.transform.position = Vector3.MoveTowards(buttonDownPos.transform.position, buttonUpPos.transform.position, pressSpeed);
+
+    }
+   
+
+}
