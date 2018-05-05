17d12cdc3a07a67f9590fe0054e7b0be82764f33
Jordan Dines <jordan@dines.net.au>
2018-05-06 00:13:51 +1000

    Got Main Menu GVR Toggle to work. Bugs in Non-VR. Canvas appears behind you

--- Assets/_Scripts/MenuManager.cs
+++ Assets/_Scripts/MenuManager.cs
@@ -19,7 +19,7 @@ public class MenuManager : MonoBehaviour {
 	private const string cardboardString = "cardboard";
 
 	void Start () {
-		StartCoroutine (LoadDevice ("None"));
+		StartCoroutine (LoadDevice ("cardboard"));
 	}
 
 	void Update ()  {
