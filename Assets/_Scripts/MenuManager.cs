8030cff4a1724597c859b3fe7fd4b473adb76126
Jordan Dines <jordan@dines.net.au>
2018-05-05 23:37:02 +1000

    some menu shit

--- Assets/_Scripts/MenuManager.cs
+++ Assets/_Scripts/MenuManager.cs
@@ -8,20 +8,30 @@ using UnityEngine.SceneManagement;
 public class MenuManager : MonoBehaviour {
 
 	[SerializeField]
-	private GameObject playerInScene;
+	private GameObject VRPlayer;
+
+
+	[SerializeField]
+	private Text deviceName;
+	[SerializeField]
+
 
 	private const string cardboardString = "cardboard";
+
+	void Update ()  {
+		deviceName.text = XRSettings.loadedDeviceName;
+	}
 
 	public void XRSwitcher ()
 	{
 		if (XRSettings.loadedDeviceName == cardboardString) {
 			XRSettings.LoadDeviceByName (cardboardString);
+			VRPlayer.GetComponent<GyroController> ().enabled = false;
 			Debug.Log (XRSettings.loadedDeviceName);
-			SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex);	
 		} else if (XRSettings.loadedDeviceName != cardboardString) {
 			XRSettings.LoadDeviceByName ("none");
-			Debug.Log (XRSettings.loadedDeviceName);
-			SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex);	
+			Debug.Log (XRSettings.loadedDeviceName + ": means nothing m9");
+			VRPlayer.GetComponent<GyroController> ().enabled = false;
 		}
 	}
 
