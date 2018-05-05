7e65b738e7ae8e553719d0ddfc5691ce8f11ebd4
Jordan Dines <jordan@dines.net.au>
2018-05-05 21:55:17 +1000

    Updated main menu scene, testing Toggle VR functionality

---
+++ Assets/_Scripts/MenuManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject playerInScene;
+
+	public Text vrSettingStateMessage;
+	private const string cardboardString = "cardboard";
+
+	public void XRSwitcher ()
+	{
+		if (XRSettings.loadedDeviceName == cardboardString) {
+			XRSettings.LoadDeviceByName (cardboardString);
+			Debug.Log (XRSettings.loadedDeviceName);
+		} else if (XRSettings.loadedDeviceName != cardboardString) {
+			XRSettings.LoadDeviceByName ("none");
+			Debug.Log (XRSettings.loadedDeviceName);
+		}
+	}
+}
