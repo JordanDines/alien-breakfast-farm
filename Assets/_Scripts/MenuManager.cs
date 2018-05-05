519a7d2c36f871b09153e9e2f44530ea089a4894
Jordan Dines <jordan@dines.net.au>
2018-05-05 23:45:46 +1000

    Fixing the menu. plz werk

--- Assets/_Scripts/MenuManager.cs
+++ Assets/_Scripts/MenuManager.cs
@@ -18,6 +18,10 @@ public class MenuManager : MonoBehaviour {
 
 	private const string cardboardString = "cardboard";
 
+	void Start () {
+		StartCoroutine (LoadDevice ("None"));
+	}
+
 	void Update ()  {
 		deviceName.text = XRSettings.loadedDeviceName;
 	}
@@ -33,6 +37,20 @@ public class MenuManager : MonoBehaviour {
 			Debug.Log (XRSettings.loadedDeviceName + ": means nothing m9");
 			VRPlayer.GetComponent<GyroController> ().enabled = false;
 		}
+	}
+
+	public void ToggleXR () {
+		if (XRSettings.loadedDeviceName == "cardboard") {
+			StartCoroutine (LoadDevice ("None"));
+		} else
+			StartCoroutine (LoadDevice ("cardboard"));
+	}
+
+	IEnumerator LoadDevice (string newDevice)
+	{
+		XRSettings.LoadDeviceByName (newDevice);
+		yield return null;
+		XRSettings.enabled = true;
 	}
 
 	public void PlayGame () 
