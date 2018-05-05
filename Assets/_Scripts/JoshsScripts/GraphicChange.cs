902ffda4d7bd7d1cb9ef6f5b6b6ca224b780d682
Joshua D'Agostino <joshua.dagostino@outlook.com>
2018-05-05 23:12:01 +1000

    --- Changes --- - Removed unused graphics options - Set up temp main menu to test switching of graphics

---
+++ Assets/_Scripts/JoshsScripts/GraphicChange.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GraphicChange : MonoBehaviour {
+
+    public string vRLevelToLoad;
+    public string phoneLevelToLoad;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void LoadNonVR()
+    {
+        {
+            // Loads the NonVR Quality Settings
+            UnityEngine.QualitySettings.SetQualityLevel(0, true);
+            Debug.Log(UnityEngine.QualitySettings.GetQualityLevel());
+            SceneManager.LoadScene(phoneLevelToLoad);
+        }
+    }
+
+    public void LoadVR()
+    {
+        {
+            // Loads the Cardboard Quality Settings
+            UnityEngine.QualitySettings.SetQualityLevel(1, true);
+            Debug.Log(UnityEngine.QualitySettings.GetQualityLevel());
+            SceneManager.LoadScene(vRLevelToLoad);
+    }
+    }
+        
+}
